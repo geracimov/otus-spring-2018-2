de262b66be8af4c59b94e2216fc163ccd8256ed7
geracimov <geracimov@gmail.com>
2019-08-27 00:47:10 +0300

    [hw20-spring-security-auth] added spring security formbased auth

---
+++ hw20-spring-security-auth/src/main/java/ru/geracimov/otus/spring/hw20springsecurityauth/domain/Author.java
@@ -0,0 +1,52 @@
+package ru.geracimov.otus.spring.hw20springsecurityauth.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "AUTHOR")
+@ToString(exclude = "books")
+public class Author {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private UUID id;
+
+    @Column(name = "NAME")
+    @NotBlank
+    private String name;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "BIRTH")
+    private LocalDate birth;
+
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private List<Book> books;
+
+    public Author(String name,
+                  LocalDate birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+}
