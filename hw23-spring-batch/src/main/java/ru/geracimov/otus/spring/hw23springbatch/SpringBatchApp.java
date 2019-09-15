2f85a94011bb7a76cfd6b70a3d901eeed0d32566
geracimov <geracimov@gmail.com>
2019-09-15 10:47:29 +0300

    [hw23-spring-batch] added module hw23-spring-batch

---
+++ hw23-spring-batch/src/main/java/ru/geracimov/otus/spring/hw23springbatch/SpringBatchApp.java
@@ -0,0 +1,14 @@
+package ru.geracimov.otus.spring.hw23springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBatchApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApp.class);
+    }
+
+}
